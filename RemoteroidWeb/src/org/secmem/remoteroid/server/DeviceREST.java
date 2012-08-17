1664c6913543fa5ec017cb2423688bec276b6dad
Taeho Kim <jyte82@gmail.com>
2012-08-17 22:45:53 +0900

    --

--- RemoteroidWeb/src/org/secmem/remoteroid/server/DeviceREST.java
+++ RemoteroidWeb/src/org/secmem/remoteroid/server/DeviceREST.java
@@ -25,7 +25,6 @@ import java.util.UUID;
 import java.util.logging.Logger;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
